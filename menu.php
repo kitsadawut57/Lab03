39763a792aec8bd60843fff148fb044a220ac7c1
Kitsadawut sahunan <6511130057@mut.ac.th>
2025-07-17 16:41:19 +0700

    commit3

--- menu.php
+++ menu.php
@@ -1,12 +1,7 @@
 <?php
     echo "Home";
     echo "Pruduct";
-<<<<<<< HEAD
+    echo "Mahanakorn";
     echo "Mahanakorn";
 
-=======
-    echo "Map";
-    echo "Add";
-echo "Pruduct";
->>>>>>> b230d17c8515a761834594b88c66020b8540b6a4
 ?>
